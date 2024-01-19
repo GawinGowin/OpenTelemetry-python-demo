012b4f0279041cf91adea64de8877d11e643a70c
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-01-19 09:20:47 +0000

    Add OpenTelemetry instrumentation for tracing and metrics

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - [Getting Started](https://opentelemetry.io/docs/instrumentation/python/getting-started/)
 - [Agent Configuration](https://opentelemetry.io/docs/instrumentation/python/automatic/agent-config/)
+- [Install the Collector | OpenTelemetry](https://opentelemetry.io/docs/collector/installation/)
--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+# These are the necessary import declarations
+from opentelemetry import trace
+from opentelemetry import metrics
+
 from random import randint
 from flask import Flask, request
 import logging
+
+# Acquire a tracer
+tracer = trace.get_tracer("diceroller.tracer")
+# Acquire a meter.
+meter = metrics.get_meter("diceroller.meter")
+
+# Now create a counter instrument to make measurements with
+roll_counter = meter.create_counter(
+    "dice.rolls",
+    description="The number of rolls by roll value",
+)
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -8,13 +23,18 @@ logger = logging.getLogger(__name__)
 
 @app.route("/rolldice")
 def roll_dice():
-    player = request.args.get('player', default = None, type = str)
-    result = str(roll())
-    if player:
-        logger.warn("%s is rolling the dice: %s", player, result)
-    else:
-        logger.warn("Anonymous player is rolling the dice: %s", result)
-    return result
+    # This creates a new span that's the child of the current one
+    with tracer.start_as_current_span("roll") as roll_span:
+        player = request.args.get('player', default = None, type = str)
+        result = str(roll())
+        roll_span.set_attribute("roll.value", result)
+        # This adds 1 to the counter for the given roll value
+        roll_counter.add(1, {"roll.value": result})
+        if player:
+            logger.warn("{} is rolling the dice: {}", player, result)
+        else:
+            logger.warn("Anonymous player is rolling the dice: %s", result)
+        return result
 
 def roll():
     return randint(1, 6)
